879a16860bed0ac70df246b3f680a707b6ca2fe5
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-09-23 13:41:51 -0300

    Added template for named.conf.options.
    This was necesary in order to specify:
    * Forwarders.
    * Networks for which recursion is allowed.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,9 @@
 default['mo_bind']['bind_dir'] = '/etc/bind'
-default['mo_bind']['cache_dir'] = '/var/cache/bind/cache'
+default['mo_bind']['cache_base_dir'] = '/var/cache/bind'
+default['mo_bind']['cache_dir'] = File.join(node['mo_bind']['cache_base_dir'], 'cache')
 default['mo_bind']['zones_dir'] = File.join(node['mo_bind']['bind_dir'], 'domain_zones')
 default['mo_bind']['zones_databag'] = :dns_zones
 default['mo_bind']['action_when_changing_zone'] = :restart
+
+default['mo_bind']['forwarders'] = %w()
+default['mo_bind']['allow_recursion'] = %w()
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,20 @@ directory node['mo_bind']['cache_dir'] do
   group 'bind'
 end
 
+template "#{node['mo_bind']['bind_dir']}/named.conf.options" do
+  source 'etc/bind/named.conf.options.erb'
+  variables ({
+    cache_base_dir: node['mo_bind']['cache_base_dir'],
+    forwarders: node['mo_bind']['forwarders'],
+    allow_recursion: node['mo_bind']['allow_recursion']
+  })
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+  notifies :restart, 'service[bind9]'
+end
+
 # Looks for every zone defined in the databag in which the server is either master or slave.
 zones = search(node['mo_bind']['zones_databag'], "masters:*#{node['ipaddress']}* OR slaves:*#{node['ipaddress']}*")
 
